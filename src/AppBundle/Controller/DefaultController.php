7dc49ec868de74724112293a787ffc78c3591205
Migdress <miguelmorcillo@unicauca.edu.co>
2018-12-22 01:09:16 -0500

    Finalizada funcionalidad

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,70 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Producto;
 
 class DefaultController extends Controller
 {
+
+    const COMANDO_AGREGAR = "Agregar";
+    const COMANDO_RESTAR = "Restar";
+    const COMANDO_ACTIVAR = "Activar";
+    const COMANDO_DESACTIVAR = "Desactivar";
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homepage", methods={"GET"})
+     * @param Request $request
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+		$productos = $this->getDoctrine()->getManager()->getRepository(Producto::class)->findAll();
+
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+			"productos"=>$productos
+		]);
+    }
+   
+    /**
+     * 
+     * @Route("/uploadFile", name="uploadcsv", methods={"POST"})
+     * @param Request $request
+     */
+    public function uploadCSVAction(Request $request){
+        $file = $request->files->get("csv");
+        $handle = fopen($file->getPathname(),"r");
+        $em = $this->getDoctrine()->getManager();
+        if($handle){
+            while(($line = fgets($handle)) !== false){
+                $lineSplit = explode(",",$line);
+                $id = trim($lineSplit[0]);
+                $accion = trim($lineSplit[1]);
+                $param = trim($lineSplit[2]);
+
+                // Se busca el producto
+                $producto = $em->getRepository(Producto::class)->find($id);
+                if(is_null($producto)){
+                    continue;
+                }
+
+                switch($accion){
+                    case self::COMANDO_ACTIVAR:
+                        $producto->activar();
+                        break;
+                    case self::COMANDO_DESACTIVAR:
+                        $producto->desactivar();
+                        break;
+                    case self::COMANDO_AGREGAR:
+                        $producto->agregar($param);
+                        break;
+                    case self::COMANDO_RESTAR:
+                        $producto->restar($param);
+                        break;
+                }
+                
+            }
+            fclose($handle);
+        }
+        $em->flush();
+        return $this->redirectToRoute("homepage");
     }
 }
--- src/AppBundle/Entity/Producto.php
+++ src/AppBundle/Entity/Producto.php
@@ -5,10 +5,14 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductoRepository")
  */
-class Product
+class Producto
 {
+
+	const ESTADO_PRODUCTO_ACTIVO = "ACTIVO";
+	const ESTADO_PRODUCTO_INACTIVO = "INACTIVO";
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -42,10 +46,192 @@ class Product
     private $unidadesActuales;
 
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="string", length=30)
      */
     private $estado;
 
 
 
+	public function __construct() {
+	}
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Producto
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set referencia
+     *
+     * @param string $referencia
+     *
+     * @return Producto
+     */
+    public function setReferencia($referencia)
+    {
+        $this->referencia = $referencia;
+
+        return $this;
+    }
+
+    /**
+     * Get referencia
+     *
+     * @return string
+     */
+    public function getReferencia()
+    {
+        return $this->referencia;
+    }
+
+    /**
+     * Set precio
+     *
+     * @param integer $precio
+     *
+     * @return Producto
+     */
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    /**
+     * Get precio
+     *
+     * @return integer
+     */
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * Set costo
+     *
+     * @param integer $costo
+     *
+     * @return Producto
+     */
+    public function setCosto($costo)
+    {
+        $this->costo = $costo;
+
+        return $this;
+    }
+
+    /**
+     * Get costo
+     *
+     * @return integer
+     */
+    public function getCosto()
+    {
+        return $this->costo;
+    }
+
+    /**
+     * Set unidadesActuales
+     *
+     * @param integer $unidadesActuales
+     *
+     * @return Producto
+     */
+    public function setUnidadesActuales($unidadesActuales)
+    {
+        $this->unidadesActuales = $unidadesActuales;
+
+        return $this;
+    }
+
+    /**
+     * Get unidadesActuales
+     *
+     * @return integer
+     */
+    public function getUnidadesActuales()
+    {
+        return $this->unidadesActuales;
+    }
+
+    /**
+     * Set estado
+     *
+     * @param integer $estado
+     *
+     * @return Producto
+     */
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+
+    /**
+     * Get estado
+     *
+     * @return integer
+     */
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+
+
+	/**
+	 *
+	 * @param integer $cantidad
+	 */
+	public function agregar($cantidad){
+		$this->setUnidadesActuales($this->getUnidadesActuales()+$cantidad);
+	}
+
+	/**
+	 *
+	 * @param integer $cantidad
+	 */
+	public function restar($cantidad){
+		$this->setUnidadesActuales($this->getUnidadesActuales()-$cantidad);
+	}
+
+	public function activar(){
+		$this->setEstado($this::ESTADO_PRODUCTO_ACTIVO);
+	}
+
+	public function desactivar(){
+		$this->setEstado($this::ESTADO_PRODUCTO_INACTIVO);
+	}
 }
